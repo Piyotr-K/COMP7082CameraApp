871ad991ddf41448db811b6d03cf545d7132e049
Piyotr-K <piyotrkao1998@Outlook.com>
2018-09-27 00:07:22 -0700

    Setup

---
+++ app/src/androidTest/java/com/example/server/cameraapp/UITests.java
@@ -0,0 +1,41 @@
+package com.example.server.cameraapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class UITests {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void TestFilter() {
+        onView(withId(R.id.btnFilter)).perform(click());
+        onView(withId(R.id.search_toDate)).perform(typeText("31/01/18"), closeSoftKeyboard());
+        onView(withId(R.id.search_fromDate)).perform(typeText("01/01/18"), closeSoftKeyboard());
+        onView(withId(R.id.search_search)).perform(click());
+        for (int i = 0; i <= 5; i++) {
+            onView(withId(R.id.btnRight)).perform(click());
+        }
+    }
+
+    @Test
+    public void TestCaption() {
+        onView(withId(R.id.entry_caption)).perform(typeText("test"));
+        onView(withId(R.id.btnCaption)).perform(click());
+        onView(withId(R.id.text_caption)).check(matches(withText("test")));
+    }
+}
---
+++ app/src/main/java/com/example/server/cameraapp/PhotoMgmtHelperClass.java
@@ -0,0 +1,20 @@
+package com.example.server.cameraapp;
+
+public class PhotoMgmtHelperClass {
+
+    public static void save() {
+
+    }
+
+    public static void remove() {
+
+    }
+
+    public static void update() {
+
+    }
+
+    public static void find(String s) {
+
+    }
+}
